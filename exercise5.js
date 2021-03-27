85d80c1e711b33800d2c698c7bd29db320ea08ab
Dario Nunez <dariose89@gmail.com>
2021-03-27 13:52:37 -0300

    exercise5.js line 19 updated from  to ''

--- exercise5.js
+++ exercise5.js
@@ -16,7 +16,7 @@ for (var i = 0; i < fiveWords.length; i++) {
 /* C- Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un 
 bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena
 completa.*/
-var sentence= "";
+var sentence= '';
 for (var i = 0; i < fiveWords.length; i++) {
   sentence= sentence + ' ' + fiveWords[i];
 }
