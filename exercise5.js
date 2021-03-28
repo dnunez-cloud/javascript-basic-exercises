9c7d600f23942d1d021242c1f5b023ac306ca904
Dario Nunez <dariose89@gmail.com>
2021-03-28 19:48:40 -0300

    updated exercise5.js, subexercise c had the string in a console log, and should be displayed in an alert

--- exercise5.js
+++ exercise5.js
@@ -20,7 +20,7 @@ var sentence= '';
 for (var i = 0; i < fiveWords.length; i++) {
   sentence= sentence + ' ' + fiveWords[i];
 }
-console.log('The string sentence is: ',sentence);
+alert('The string sentence is: '+ sentence);
 
 /* D- Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, es 
 decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al
